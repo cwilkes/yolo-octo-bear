341a916a379ad15956df90fd090b6e33f2e2891f
Chris Wilkes <cwilkes@bluekai.com>
2014-06-27 07:05:49 -0700

    add scaling image data

--- collage_maker.py
+++ collage_maker.py
@@ -1,4 +1,5 @@
 import sys
+from collections import defaultdict
 
 
 def lgi(msg, *args):
@@ -32,9 +33,53 @@ class Tile(object):
     def __init__(self, image, top_col=-1, top_row=-1, width=-1, height=-1):
         self.image = image
         self.top_col, self.top_row, self.width, self.height = top_col, top_row, width, height
+        self.cached_data = defaultdict(dict)
+
+    def move(self, top_col, top_row):
+        self.top_col, self.top_row = top_col, top_row
+
+    def _is_off_board(self):
+        return self.top_col == -1 or self.width == -1
+
+    def data(self):
+        if self._is_off_board():
+            return None
+        if self.width in self.cached_data and self.height in self.cached_data[self.width]:
+            return self.cached_data[self.width][self.height]
+        ret = list()
+        ratio_w = 1.0 * self.image.width / self.width
+        ratio_h = 1.0 * self.image.height / self.height
+        ratio_w_int, ratio_h_int = int(ratio_w), int(ratio_h)
+        top_r = 0
+        for r1 in range(self.height):
+            row = list()
+            top_c = 0
+            for c1 in range(self.width):
+                count, color_sum = 0, 0
+                for r2 in range(ratio_h_int):
+                    for c2 in range(ratio_w_int):
+                        try:
+                            color_sum += self.image[int(top_r+r1)][int(top_c+c2)]
+                            count += 1
+                        except:
+                            pass
+                # should really do the next fraction
+                if count > 0:
+                    row.append(color_sum / count)
+                else:
+                    lgi('No nodes at (%d,%d) : (%d,%d) + (%d,%d)', c1, r1, top_r, top_c, c2, r2)
+                top_c += ratio_w
+            ret.append(row)
+            top_r += ratio_h
+        if ret:
+            lgi('Size for %s is (%d,%d)', self.image, len(ret[0]), len(ret))
+            self.cached_data[self.width][self.height] = ret
+            return ret
+        else:
+            raise Exception('No data available for image %s with size (%d,%d)', self.image, self.width, self.height)
 
     def as_ary(self):
-        if self.top_col == -1:
+        if self._is_off_board():
             return [-1, -1, -1, -1]
         else:
             return [self.top_row, self.top_col, self.top_row+self.height, self.top_col+self.width]
@@ -45,6 +90,12 @@ class Image(object):
         self.index = index
         self.data = data
         self.width, self.height = len(self.data[0]), len(self.data)
+
+    def __getitem__(self, item):
+        try:
+            return self.data[item]
+        except IndexError as ex:
+            raise Exception('Cannot get item %d out of data of size %d : %s' % (item, len(self.data), ex))
 
     def __repr__(self):
         return '<image:%d %dx%d>' % (self.index, self.width, self.height)
@@ -75,6 +126,9 @@ def do_work(source_image, images):
             index += 1
             col += width + 1
         row += height + 1
+    # now rescale all their image sizes
+    for tile in board.tiles:
+        tile.data()
     return board.as_ary()
 
 
@@ -116,6 +170,7 @@ def do_check(ret, images):
                     lgi('Error at (%d,%d) piece %d (%s) already exists, cannot put %d (%s) on it',
                         c, r, row[c], images[row[c]-1], piece, images[piece-1])
                 row[c] = piece
+
 
 def compose(image_collection):
     source_image, images = make_images(image_collection)
